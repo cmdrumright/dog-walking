35e4f3efde1bd114d8568821911703c766a5718f
cmdrumright <cdrumright@protonmail.com>
2025-11-24 15:18:24 -0600

    Added click event listener to Walkers

--- scripts/Walkers.js
+++ scripts/Walkers.js
@@ -2,6 +2,10 @@ import { getWalkers } from "./database.js"
 
 const walkers = getWalkers()
 
+document.addEventListener("click", (clickEvent) => {
+    const clickedElement = clickEvent.target;
+    window.alert(`${clickedElement.dataset.city}`)
+})
 
 export const Walkers = () => {
     let walkerHTML = "<ul>"
