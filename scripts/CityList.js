525dfd65b6326a3388a4c2da7e596c7ef47d4649
cmdrumright <cdrumright@protonmail.com>
2025-11-25 11:49:45 -0600

    updated CityList to use city data and search walkers for clicked city id

--- scripts/CityList.js
+++ scripts/CityList.js
@@ -1,19 +1,22 @@
-import { getWalkers } from "./database.js"
-
-const walkers = getWalkers()
+import { getCities, getWalkers } from "./database.js"
 
 document.addEventListener("click", (clickEvent) => {
     const clickTarget = clickEvent.target;
     if (clickTarget.dataset.type === "city") {
-        window.alert(`${clickTarget.dataset.walkername} is servicing this city`)
+        // use city id to get list of walkers in that city
+        const allWalkers = getWalkers();
+        const foundWalkers = allWalkers.filter(({cityId}) => cityId === parseInt(clickTarget.dataset.id));
+        let walkersString = "These walkers are servicing this city:\n";
+        walkersString += foundWalkers.map((walker) => walker.name).join("\n");
+        window.alert(walkersString);
     }
 })
 
 export const CityList = () => {
+    const cities = getCities();
     let citiesHTML = "<ul>"
-
-    for (const walker of walkers) {
-        citiesHTML += `<li data-type="city" data-walkername="${walker.name}">${walker.city}</li>`
+    for (const city of cities) {
+        citiesHTML += `<li data-type="city" data-id="${city.id}">${city.name}</li>`
     }
 
     citiesHTML += "</ul>"
