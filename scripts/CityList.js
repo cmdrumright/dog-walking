f8fc2270ee6c7d2a3423735456c577895067c1d2
cmdrumright <cdrumright@protonmail.com>
2025-11-25 09:55:45 -0600

    added data to city list items and click event listener to display walkername when city is clicked

--- scripts/CityList.js
+++ scripts/CityList.js
@@ -2,12 +2,18 @@ import { getWalkers } from "./database.js"
 
 const walkers = getWalkers()
 
+document.addEventListener("click", (clickEvent) => {
+    const clickTarget = clickEvent.target;
+    if (clickTarget.dataset.type === "city") {
+        window.alert(`${clickTarget.dataset.walkername} is servicing this city`)
+    }
+})
 
 export const CityList = () => {
     let citiesHTML = "<ul>"
 
     for (const walker of walkers) {
-        citiesHTML += `<li>${walker.city}</li>`
+        citiesHTML += `<li data-type="city" data-walkername="${walker.name}">${walker.city}</li>`
     }
 
     citiesHTML += "</ul>"
