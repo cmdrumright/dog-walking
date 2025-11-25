552517ea9fe426ad170e8cbb4e53ff5b8f1dbe41
cmdrumright <cdrumright@protonmail.com>
2025-11-25 11:58:37 -0600

    updated Assignments to use cityData

--- scripts/Assignments.js
+++ scripts/Assignments.js
@@ -1,8 +1,4 @@
-import { getPets, getWalkers } from "./database.js"
-
-// Get copy of state for use in this module
-const pets = getPets()
-const walkers = getWalkers()
+import { getPets, getWalkers, getCities } from "./database.js"
 
 
 // Function whose responsibility is to find the walker assigned to a pet
@@ -18,16 +14,34 @@ const findWalker = (pet, allWalkers) => {
     return petWalker
 }
 
+const findCity = (walker, allCities) => {
+    let walkerCity = null
+
+    for (const city of allCities) {
+        if (city.id === walker.cityId) {
+            walkerCity = city
+        }
+    }
+
+    return walkerCity
+}
+
 export const Assignments = () => {
+    // Get copy of state for use in this module
+    const pets = getPets()
+    const walkers = getWalkers()
+    const cities = getCities()
+
     let assignmentHTML = ""
     assignmentHTML = "<ul>"
 
     for (const currentPet of pets) {
         const currentPetWalker = findWalker(currentPet, walkers)
+        const currentCity = findCity(currentPetWalker, cities)
         assignmentHTML += `
             <li>
                 ${currentPet.name} is being walked by
-                ${currentPetWalker.name} in ${currentPetWalker.city}
+                ${currentPetWalker.name} in ${currentCity.name}
             </li>
         `
     }
